b9945e1b2bce0b18cc90ba85d45715ccf44fad1c
Yan Kapiankov <g00399001@atu.ie>
2025-04-22 17:28:07 +0100

    Figured out problem was calling res.categories instead of res.meals so filter wasn't working properly.

--- src/app/recipes/recipes.page.ts
+++ src/app/recipes/recipes.page.ts
@@ -1,35 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
-import { HttpClient } from '@angular/common/http';
-import { OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-recipes',
   standalone: true,
-  imports: [
-    CommonModule,
-    IonicModule, 
-    RouterModule
-  ],
+  imports: [CommonModule, IonicModule, RouterModule],
   templateUrl: './recipes.page.html',
   styleUrls: ['./recipes.page.scss']
 })
 export class RecipesPage implements OnInit {
   meals: any[] = [];
+  categories: string[] = [];
+  selectedCategory: string = '';
 
   constructor(private http: HttpClient) {}
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.getCategories();
     this.loadMeals();
   }
 
-  loadMeals() {
-    this.http.get<any>('https://www.themealdb.com/api/json/v1/1/search.php?s=') //API FOR MEALS/RECIPES
+  getCategories() {
+    this.http.get<any>('https://www.themealdb.com/api/json/v1/1/list.php?c=list')
+      .subscribe(res => {
+        console.log('Categories:', res); 
+        this.categories = res.meals?.map((cat: any) => cat.strCategory) || [];
+      });
+  }
+  
+  
+
+  loadMeals(category?: string) {
+    const url = category
+      ? `https://www.themealdb.com/api/json/v1/1/filter.php?c=${category}`
+      : 'https://www.themealdb.com/api/json/v1/1/search.php?s=';
+
+    this.http.get<any>(url)
       .subscribe(res => {
         this.meals = res.meals || [];
-        console.log(this.meals); 
       });
+  }
+
+  selectCategory(cat: string) {
+    this.selectedCategory = cat;
+    this.loadMeals(cat);
   }
 }
