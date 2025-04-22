17c0a653cc46d1d0dbd4c994f139d82d1fe946bd
Yan Kapiankov <g00399001@atu.ie>
2025-04-22 17:46:04 +0100

    Added recipe-modal for popup recipes when card clicked

---
+++ src/app/components/recipe-modal/recipe-modal.component.ts
@@ -0,0 +1,41 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { ModalController, IonicModule } from '@ionic/angular';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-recipe-modal',
+  standalone: true,
+  imports: [CommonModule, IonicModule],
+  templateUrl: './recipe-modal.component.html',
+  styleUrls: ['./recipe-modal.component.scss']
+})
+export class RecipeModalComponent implements OnInit {
+  @Input() mealId!: string;
+  meal: any;
+
+  constructor(private http: HttpClient, private modalCtrl: ModalController) {}
+
+  ngOnInit() {
+    this.http.get<any>(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${this.mealId}`)
+      .subscribe(res => {
+        this.meal = res.meals[0];
+      });
+  }
+
+  closeModal() {
+    this.modalCtrl.dismiss();
+  }
+
+  getIngredients() {
+    const ingredients: string[] = [];
+    for (let i = 1; i <= 20; i++) {
+      const ingredient = this.meal[`strIngredient${i}`];
+      const measure = this.meal[`strMeasure${i}`];
+      if (ingredient) {
+        ingredients.push(`${measure} ${ingredient}`);
+      }
+    }
+    return ingredients;
+  }
+}
--- src/app/recipes/recipes.page.ts
+++ src/app/recipes/recipes.page.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
-import { IonicModule } from '@ionic/angular';
+import { IonicModule, ModalController } from '@ionic/angular';
 import { RouterModule } from '@angular/router';
+import { RecipeModalComponent } from '../components/recipe-modal/recipe-modal.component';
 
 @Component({
   selector: 'app-recipes',
@@ -16,7 +17,10 @@ export class RecipesPage implements OnInit {
   categories: string[] = [];
   selectedCategory: string = '';
 
-  constructor(private http: HttpClient) {}
+  constructor(
+    private http: HttpClient,
+    private modalCtrl: ModalController
+  ) {}
 
   ngOnInit() {
     this.getCategories();
@@ -26,12 +30,10 @@ export class RecipesPage implements OnInit {
   getCategories() {
     this.http.get<any>('https://www.themealdb.com/api/json/v1/1/list.php?c=list')
       .subscribe(res => {
-        console.log('Categories:', res); 
+        // TheMealDB returns 'meals' for category list
         this.categories = res.meals?.map((cat: any) => cat.strCategory) || [];
       });
   }
-  
-  
 
   loadMeals(category?: string) {
     const url = category
@@ -47,5 +49,13 @@ export class RecipesPage implements OnInit {
   selectCategory(cat: string) {
     this.selectedCategory = cat;
     this.loadMeals(cat);
+  }
+
+  async openRecipeModal(mealId: string) {
+    const modal = await this.modalCtrl.create({
+      component: RecipeModalComponent,
+      componentProps: { mealId }
+    });
+    await modal.present();
   }
 }
